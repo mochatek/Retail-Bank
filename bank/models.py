4f4235ba4a05054bc289421dc23dfaf621ee5cf9
mochatek <akashsabu@ymail.com>
2020-06-12 17:48:11 +0530

    auth completed

--- bank/models.py
+++ bank/models.py
@@ -1,1 +1,24 @@
-from bank import app
+from bank import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ssnid = db.Column(db.Integer, index=True, unique=True)
+    uname = db.Column(db.String(50), index=False, unique=False)
+    age = db.Column(db.Integer, index=False, unique=False)
+    addr1 = db.Column(db.Text, index=False, unique=False)
+    addr2 = db.Column(db.Text, index=False, unique=False)
+    city = db.Column(db.String(50), index=False, unique=False)
+    state = db.Column(db.String(50), index=False, unique=False)
+    is_cust = db.Column(db.Integer, index=False, unique=False)
+    created = db.Column(db.Date, index=False, unique=False)
+    password = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password= generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return 'User : {}'.format(self.uname)
--- bank/routes.py
+++ bank/routes.py
@@ -1,14 +1,48 @@
 from bank import app
 from flask import render_template, request, redirect, url_for, flash
+from bank.models import User, db
+from datetime import datetime
+from sqlalchemy import and_
 
 @app.route('/')
 def index():
     return render_template('index.html', page='Index')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', page='Login')
+    if request.method == 'GET':
+        return render_template('login.html', page='Login')
+    else:
+        uname = request.form.get('uname')
+        password = request.form.get('password')
+        is_cust = int(request.form.get('is_cust'))
+        user = db.session.query(User).filter(User.uname==uname, User.is_cust==is_cust).first()
+        if user and user.check_password(password):
+            flash('Successfully Logged in.')
+            return redirect(url_for('index'))
+        else:
+            flash('Username-Password mismatch !!')
+        return redirect(url_for('login'))
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', page='Register')
+    if request.method == 'GET':
+        return render_template('register.html', page='Register')
+    else:
+        ssnid = request.form.get('ssnid')
+        uname = request.form.get('uname')
+        age = request.form.get('age')
+        addr1 = request.form.get('addr1')
+        addr2 = request.form.get('addr2')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        is_cust = 1                         #Used to distinguish between tellet & customer.
+        created = datetime.now()            #Timestamp of creation.
+        password = request.form.get('password')
+        user = User(ssnid=ssnid, uname=uname, age=age, addr1=addr1,
+                    addr2=addr2, city=city, state=state, is_cust=is_cust, created=created)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successfull ! Login to continue.')
+        return redirect(url_for('login'))
