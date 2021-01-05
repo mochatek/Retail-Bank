6f558b56a914fa72478106bd9ff463cbefd801ec
Akash S Panickar <akashsabu@ymail.com>
2021-01-05 20:40:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from os import path, environ
 basedir = path.abspath(path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = environ.get('SECRET_KEY') or 'TCS_Case_Study_1'
+    SECRET_KEY = environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:///' + path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
